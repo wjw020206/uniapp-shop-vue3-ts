0e73b9fc84fc4a6cefb52182d70600544df5f53f
CodePencil <jiawei.wang2021@gmail.com>
2026-01-22 22:58:08 +0800

    refactor: 去除了无用的类型声明

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,10 +1,1 @@
 /// <reference types="vite/client" />
-
-/* eslint-disable @typescript-eslint/no-empty-object-type */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-declare module '*.vue' {
-  import { DefineComponent } from 'vue'
-  const component: DefineComponent<{}, {}, any>
-  export default component
-}
