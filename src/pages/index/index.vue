e1bb563cacbc0c7b645d22724d815af540ed1d5c
CodePencil <jiawei.wang2021@gmail.com>
2026-01-23 13:15:33 +0800

    feat: 完成了热门推荐的组件和接口对接

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,26 +1,61 @@
 <template>
+  <!-- 自定义导航栏 -->
   <CustomNavbar />
+  <!-- 自定义轮播图 -->
   <XtxSwiper :list="bannerList" />
+  <!-- 分类面板 -->
+  <CategoryPanel :list="categoryList" />
+  <!-- 热门推荐 -->
+  <HotPanel :list="hotList" />
   <view class="index"></view>
 </template>
 
 <script lang="ts" setup>
-import { getHomeBannerAPI } from '@/services/home'
+import {
+  getHomeBannerAPI,
+  getHomeCategoryAPI,
+  getHomeHotAPI,
+} from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
+import CategoryPanel from './components/CategoryPanel.vue'
+import HotPanel from './components/HotPanel.vue'
 
+/** 轮播图数据 */
 const bannerList = ref<BannerItem[]>([])
-
+/** 获取轮播图数据 */
 const getHomeBannerData = async () => {
   const res = await getHomeBannerAPI()
   bannerList.value = res.result
 }
 
+/** 前台分类数据 */
+const categoryList = ref<CategoryItem[]>([])
+/** 获取前台分类数据 */
+const getHomeCategoryData = async () => {
+  const res = await getHomeCategoryAPI()
+  categoryList.value = res.result
+}
+
+/** 热门推荐数据 */
+const hotList = ref<HotItem[]>([])
+/** 获取热门推荐数据 */
+const getHomeHotData = async () => {
+  const res = await getHomeHotAPI()
+  hotList.value = res.result
+}
+
 onLoad(() => {
   getHomeBannerData()
+  getHomeCategoryData()
+  getHomeHotData()
 })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss">
+page {
+  background-color: #f7f7f7;
+}
+</style>
--- src/services/home.ts
+++ src/services/home.ts
@@ -1,7 +1,7 @@
-import type { BannerItem } from '@/types/home'
+import type { BannerItem, CategoryItem, HotItem } from '@/types/home'
 import { http } from '@/utils/http'
 
-/** 获取首页轮播图
+/** 首页-广告区域-小程序
  * @param distributionSite 广告区域展示位置（1 为首页，2 为分类商品页）
  */
 export const getHomeBannerAPI = (distributionSite = 1) => {
@@ -11,5 +11,23 @@ export const getHomeBannerAPI = (distributionSite = 1) => {
     data: {
       distributionSite,
     },
+  })
+}
+
+/** 首页-前台分类-小程序
+ */
+export const getHomeCategoryAPI = () => {
+  return http<CategoryItem[]>({
+    method: 'GET',
+    url: '/home/category/mutli',
+  })
+}
+
+/** 首页-热门推荐-小程序
+ */
+export const getHomeHotAPI = () => {
+  return http<HotItem[]>({
+    method: 'GET',
+    url: '/home/hot/mutli',
   })
 }
