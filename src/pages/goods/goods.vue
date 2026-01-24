3a2bb53fe8eef43739aedb2cf7942e9f77454513
CodePencil <jiawei.wang2021@gmail.com>
2026-01-24 13:10:32 +0800

    feat: 完成了商品详情弹出层交互的功能

--- src/pages/goods/goods.vue
+++ src/pages/goods/goods.vue
@@ -37,11 +37,11 @@
           <text class="label">选择</text>
           <text class="text ellipsis">请选择商品规格</text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('address')" class="item arrow">
           <text class="label">送至</text>
           <text class="text ellipsis">请选择收获地址</text>
         </view>
-        <view class="item arrow">
+        <view @tap="openPopup('service')" class="item arrow">
           <text class="label">服务</text>
           <text class="text ellipsis">无忧退 快速退款 免费包邮</text>
         </view>
@@ -125,6 +125,12 @@
       <view class="payment"> 立即购买 </view>
     </view>
   </view>
+
+  <!-- uni-ui 弹出层 -->
+  <uni-popup ref="popup" type="bottom" background-color="#fff">
+    <AddressPanel v-if="popupName === 'address'" @close="popup?.close" />
+    <ServicePanel v-if="popupName === 'service'" @close="popup?.close" />
+  </uni-popup>
 </template>
 
 <script setup lang="ts">
@@ -132,6 +138,8 @@ import { getGoodsByIdAPI } from '@/services/goods'
 import type { GoodsResult } from '@/types/goods'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
+import AddressPanel from './components/AddressPanel.vue'
+import ServicePanel from './components/ServicePanel.vue'
 
 /** 获取屏幕边界到安全区域距离 */
 const { safeAreaInsets } = uni.getSystemInfoSync()
@@ -167,6 +175,18 @@ const onTapImage = (url: string) => {
     current: url,
     urls: goods.value!.mainPictures,
   })
+}
+
+/** 弹出层实例 */
+const popup = ref<UniHelper.UniPopupInstance>()
+/** 弹出层条件渲染 */
+const popupName = ref<'address' | 'service'>()
+/** 打开弹出层
+ * @param name 类型名称
+ */
+const openPopup = (name: typeof popupName.value) => {
+  popupName.value = name
+  popup.value?.open?.()
 }
 </script>
 
